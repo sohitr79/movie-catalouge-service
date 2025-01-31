63ecee62a53480a5ebd6b883fc6644db047ee64a
sohit <sohitr79@gmail.com>
2025-02-01 03:24:08 +0530

    Using webClient instead of Rest Template

--- src/main/java/com/incubation/movie_catalouge_service/config/AppConfig.java
+++ src/main/java/com/incubation/movie_catalouge_service/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.incubation.movie_catalouge_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +11,10 @@ public class AppConfig {
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient.Builder getWebClientBuilder(){
+        return WebClient.builder();
     }
 }
--- src/main/java/com/incubation/movie_catalouge_service/controller/MovieCatalogController.java
+++ src/main/java/com/incubation/movie_catalouge_service/controller/MovieCatalogController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -20,20 +21,41 @@ import java.util.stream.Collectors;
 public class MovieCatalogController
 {
     @Autowired
-    RestTemplate restTemplate;
+    RestTemplate restTemplate; //Is going to be depriciated near soon
+
+    @Autowired
+    WebClient.Builder webClientBuilder; //This is new way of calling microservices
 
     @GetMapping("/{userId}")
     public List<CatlogItems> getCatalogItems(@PathVariable("userId") String id){
 
         List<Rating> ratings = Arrays.asList(new Rating("1234",4),new Rating("532",5));
 
-        List<CatlogItems> catlogItems = ratings.stream().map(
-                rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+//        //Using Rest Teamplate to call other service
+//        List<CatlogItems> catlogItems = ratings.stream().map(
+//                rating -> {
+//                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+//                    return new CatlogItems(movie.getMovieName(), movie.getMovieDescription(), rating.getRating());
+//                }
+//        ).collect(Collectors.toList());
+
+        //Using WebClient to call other service
+
+        List<CatlogItems> catlogItems1 = ratings.stream().map(rating -> {
+                    Movie movie = webClientBuilder.build()
+                            .get() //tells it's a get call
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            .retrieve() //collect what ever is returned by URI
+                            .bodyToMono(Movie.class) //Mono is promise that its going to return something
+                            .block(); //Since this call is Async we are blocking further execution until mono is return
                     return new CatlogItems(movie.getMovieName(), movie.getMovieDescription(), rating.getRating());
                 }
+
         ).collect(Collectors.toList());
 
-        return catlogItems;
+
+//        return catlogItems;
+        return catlogItems1;
     }
 }
