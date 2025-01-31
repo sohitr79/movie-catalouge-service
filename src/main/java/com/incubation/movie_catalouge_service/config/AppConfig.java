09764d294c14530af74357a5b76c2d97f9550682
sohit <sohitr79@gmail.com>
2025-02-01 03:01:30 +0530

    Moving RestTemplate initialization from controller's method to AppConfig so only one bean is created

---
+++ src/main/java/com/incubation/movie_catalouge_service/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.incubation.movie_catalouge_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
--- src/main/java/com/incubation/movie_catalouge_service/controller/MovieCatalogController.java
+++ src/main/java/com/incubation/movie_catalouge_service/controller/MovieCatalogController.java
@@ -3,6 +3,7 @@ package com.incubation.movie_catalouge_service.controller;
 import com.incubation.movie_catalouge_service.model.CatlogItems;
 import com.incubation.movie_catalouge_service.model.Movie;
 import com.incubation.movie_catalouge_service.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +19,13 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogController
 {
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/{userId}")
     public List<CatlogItems> getCatalogItems(@PathVariable("userId") String id){
 
-        RestTemplate restTemplate = new RestTemplate();
-
         List<Rating> ratings = Arrays.asList(new Rating("1234",4),new Rating("532",5));
-
-//        Movie forObject = restTemplate.getForObject("http://localhost:8082/movies/{movieId}", Movie.class);
 
         List<CatlogItems> catlogItems = ratings.stream().map(
                 rating -> {
