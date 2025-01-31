d6ea6d03959c3feb134a0ede5ce91b6cd521c446
sohit <sohitr79@gmail.com>
2025-02-01 02:51:11 +0530

    Using Resttemplate to call MovieInfo Service

--- src/main/java/com/incubation/movie_catalouge_service/controller/MovieCatalogController.java
+++ src/main/java/com/incubation/movie_catalouge_service/controller/MovieCatalogController.java
@@ -1,13 +1,18 @@
 package com.incubation.movie_catalouge_service.controller;
 
 import com.incubation.movie_catalouge_service.model.CatlogItems;
+import com.incubation.movie_catalouge_service.model.Movie;
+import com.incubation.movie_catalouge_service.model.Rating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -16,6 +21,20 @@ public class MovieCatalogController
 
     @GetMapping("/{userId}")
     public List<CatlogItems> getCatalogItems(@PathVariable("userId") String id){
-        return Collections.singletonList(new CatlogItems("DDLJ","LoveStory",5));
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(new Rating("1234",4),new Rating("532",5));
+
+//        Movie forObject = restTemplate.getForObject("http://localhost:8082/movies/{movieId}", Movie.class);
+
+        List<CatlogItems> catlogItems = ratings.stream().map(
+                rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatlogItems(movie.getMovieName(), movie.getMovieDescription(), rating.getRating());
+                }
+        ).collect(Collectors.toList());
+
+        return catlogItems;
     }
 }
---
+++ src/main/java/com/incubation/movie_catalouge_service/model/Movie.java
@@ -0,0 +1,28 @@
+package com.incubation.movie_catalouge_service.model;
+
+public class Movie {
+
+
+    private String movieId;
+    private String movieName;
+    private String movieDescription;
+
+
+    public Movie(String movieId, String movieName, String movieDescription) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.movieDescription = movieDescription;
+    }
+
+
+    public String getMovieId() {
+        return movieId;
+    }
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public String getMovieDescription() {
+        return movieDescription;
+    }
+}
