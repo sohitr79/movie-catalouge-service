8246c4dc44b7a6b1de9d58122de453dad8a59398
sohit <sohitr79@gmail.com>
2025-02-01 17:41:30 +0530

    setting timeout on RestTemplate

--- src/Readme.md
+++ src/Readme.md
@@ -1,4 +1,4 @@
 Execrise 1:
 Add an API to movie catalog service at /catalog/{userId} that returns a hardcoded list of movie + rating information
 
-![img.png](img.png)
+![img_1.png](img_1.png)
--- src/main/java/com/incubation/movie_catalouge_service/config/AppConfig.java
+++ src/main/java/com/incubation/movie_catalouge_service/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.incubation.movie_catalouge_service.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.reactive.HttpComponentsClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,7 +14,9 @@ public class AppConfig {
     @Bean
     @LoadBalanced // with loadBalenced annotation it will use Eureka server
     public RestTemplate getRestTemplate(){
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000); //setting timeout to Rest Template
+        return new RestTemplate(clientHttpRequestFactory);
     }
 
     @Bean
