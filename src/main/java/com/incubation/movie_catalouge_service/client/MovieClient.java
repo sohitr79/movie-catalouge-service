279fb2bad56660146fdf5126163cb0f1163c0a0a
sohit <sohitr79@gmail.com>
2025-03-25 21:09:25 +0530

    fix movie client and rating data

--- src/main/java/com/incubation/movie_catalouge_service/client/MovieClient.java
+++ src/main/java/com/incubation/movie_catalouge_service/client/MovieClient.java
@@ -5,6 +5,7 @@ import com.incubation.movie_catalouge_service.model.Movie;
 import com.incubation.movie_catalouge_service.model.UserRating;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class MovieClient {
 
     private static final String MOVIE_CATALOG_SERVICE = "movieCatalogService";
--- src/main/java/com/incubation/movie_catalouge_service/client/RatingsClient.java
+++ src/main/java/com/incubation/movie_catalouge_service/client/RatingsClient.java
@@ -4,10 +4,12 @@ import com.incubation.movie_catalouge_service.model.Rating;
 import com.incubation.movie_catalouge_service.model.UserRating;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
+@Component
 public class RatingsClient {
     private static final String MOVIE_CATALOG_SERVICE = "movieCatalogService";
     @Autowired
